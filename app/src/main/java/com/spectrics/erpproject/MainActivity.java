e037a5ffc35d91c12da3186d24335cdd1928cdc2
Disha <pdisha944@gmail.com>
2023-11-26 17:51:27 +0530

    Added bottom navigation bar layout

--- app/src/main/java/com/spectrics/erpproject/MainActivity.java
+++ app/src/main/java/com/spectrics/erpproject/MainActivity.java
@@ -4,12 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.FirebaseApp;
 
-;
 
 public class MainActivity extends AppCompatActivity {
     private static final int SPLASH_SCREEN_TIME_OUT = 2000; // After completion of 2000 ms, the next activity will get started.
@@ -17,13 +18,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        BottomNavigationView bottomNavigationView;
         // This method is used so that your splash activity can cover the entire screen.
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_splash); // this will bind your MainActivity.class file with activity_main.
         FirebaseApp.initializeApp(getApplicationContext());
+        BottomNavigationView navLay = findViewById(R.id.bottom_navigation);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
